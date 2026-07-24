3fb256f66712fa198721743c3c1c74abad3545ee
ddon <ddonjonathan@gmail.com>
2026-07-24 12:52:19 +0100

    added netlify domain in api

--- backend/api.py
+++ backend/api.py
@@ -29,7 +29,7 @@ app = FastAPI(title="Tokflow Core API")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", "https://tokkflow.netlify.app/"], 
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "https://tokkflow.netlify.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
